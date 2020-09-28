40c243e173a60f1f96e8dedb82e5b4e0312985e8
halimenur <44686570+halimenur@users.noreply.github.com>
2020-09-28 20:39:13 +0300

    Add files via upload

---
+++ Inheritance/src/com/company/Customer.java
@@ -0,0 +1,8 @@
+package com.company;
+//Customer miras alır persondan
+public class Customer extends  Person {
+
+    String email;
+
+
+}
---
+++ Inheritance/src/com/company/EmployeeManager.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class EmployeeManager  extends  PersonManager{
+
+    public void ayElemani(){
+
+        System.out.println("Ayın elemanı getirildi");
+    }
+}
